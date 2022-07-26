7bf49b782610c6d2e5416d19d2cbc8f17860ee54
bencyphyr <ben.villalba@gmail.com>
2022-07-26 12:27:37 +0800

    Update database_access.py
    - Added remove_session() function.
    - Added two more fields to the table, session_cost and duration. Table will be required to be cleared out from the database.

--- database_access.py
+++ database_access.py
@@ -23,6 +23,8 @@ def check_db(): # Progress: Complete
         license_number VARCHAR(25),
         session_start DATETIME,
         session_end DATETIME,
+        session_cost DECIMAL(30, 4),
+        duration INT(500),
         lot_number INT(500)
         );
         """)
@@ -31,16 +33,18 @@ def check_db(): # Progress: Complete
     else:
         print('Database already exists.')
 
+
 ''' 
 Takes in a tuple of session details containing: 
 License Number in string
 Session Start in datetime
 Session End in datetime
+Session Cost in Decimal
+Duration in integer
 Lot Number in integer
 
 Adds the tuple of session details into the database.
 Commits after change is done then close the connection.
-
 '''
 def add_session(session): # Progress: Complete
     # Creates a parking.db file if it does not exist, otherwise accesses it if exists
@@ -50,11 +54,28 @@ def add_session(session): # Progress: Complete
     # Note: All SQL commands are done with the cursor object.
     cur = con.cursor()
 
-    sql_command = "INSERT INTO sessions VALUES(?, ?, ?, ?)"
+    sql_command = "INSERT INTO sessions VALUES(?, ?, ?, ?, ?, ?)"
     cur.execute(sql_command, session)
 
     con.commit()
     con.close()
+
+
+def remove_session(plate_number): # Progress: WIP
+    # Creates a parking.db file if it does not exist, otherwise accesses it if exists
+    con = sql.connect(database_name)
+
+    # Create a cursor object to access table
+    # Note: All SQL commands are done with the cursor object.
+    cur = con.cursor()
+
+    # Getting previous sessions
+    sql_command = "DELETE sessions WHERE license_number = ?"
+    cur.execute(sql_command, (plate_number))
+
+    con.commit()
+    con.close()
+
 
 # Takes in user's plate number and finds any records in the database containing that plate number
 def get_previous_sessions(plate_number): # Progress: Complete
@@ -66,7 +87,7 @@ def get_previous_sessions(plate_number): # Progress: Complete
     cur = con.cursor()
     
     # Getting previous sessions
-    cur.execute("SELECT * FROM sessions WHERE license_number == ?", (plate_number))
+    cur.execute("SELECT * FROM sessions WHERE license_number = ?", (plate_number))
     
     prev_sessions = cur.fetchall()
 
